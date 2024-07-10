798706f23ba5fd2b9b7a38e976751dd07843619b
schech1 <76683319+schech1@users.noreply.github.com>
2024-07-10 16:36:31 +0200

    Logging fixes

--- SDK/Controller/Logging.swift
+++ SDK/Controller/Logging.swift
@@ -1,54 +1,68 @@
 import OSLog
 import Foundation
-import UIKit
+
 
 public class Logging {
     
     public static let shared = Logging()
-    private let config = Config.shared
+    let config = Config.shared
     
-    private init() {}
     
     public func log(type: LogType, _ message: String, functionName: String = #function) {
+        let date = Date()
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd/MM/yy HH:mm:ss"
-        let dateString = dateFormatter.string(from: Date())
+        let dateString = dateFormatter.string(from: date)
         let content = "[\(type)] \(dateString): \(functionName) - \(message)\n"
         
         // Log to OS
         Logger.debug.error("\(content)")
         
         // Optionally log to file
-        if config.logToFile {
+        if config.logToFile{
             logToFile(content)
         }
     }
     
     private func logToFile(_ content: String) {
-        let fileURL = getDocumentsDirectory().appendingPathComponent("log.txt")
+        let documentDirectory = getDocumentsDirectory()
+        let fileURL = documentDirectory.appendingPathComponent("log.txt")
+        
+        if !FileManager.default.fileExists(atPath: fileURL.path) {
+            FileManager.default.createFile(atPath: fileURL.path, contents: nil, attributes: nil)
+        }
         
         do {
             let previousContent = try String(contentsOf: fileURL)
+            
             let newContent = content + previousContent
+            
             try newContent.write(to: fileURL, atomically: true, encoding: .utf8)
         } catch {
             print("Error writing to log file: \(error)")
         }
     }
     
-    private func getDocumentsDirectory() -> URL {
-        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-    }
-    
     public func clearLogFile() {
-        let logFilePath = getDocumentsDirectory().appendingPathComponent("log.txt")
+        let documentDirectory = getDocumentsDirectory()
+        let logFilePath = documentDirectory.appendingPathComponent("log.txt")
         do {
             try FileManager.default.removeItem(at: logFilePath)
         } catch {
             print("Error clearing log file: \(error.localizedDescription)")
         }
     }
+    
 }
+
+
+
+private func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+}
+
+
 
 extension Logger {
     private static let subsystem = Bundle.main.bundleIdentifier!
